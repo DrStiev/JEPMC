4f7a250b3d5c071b5803f0b3d0fecf7402fceb90
DrStiev <55959491+DrStiev@users.noreply.github.com>
2023-04-04 09:26:06 +0200

    modeling seirs with dynamics

--- src/ode.jl
+++ src/ode.jl
@@ -6,24 +6,31 @@ module ode_model
 	using ModelingToolkit, DifferentialEquations, OrdinaryDiffEq
 	using Plots, LaTeXStrings
 
-	function seirs!(du, u, p ,t)
+	# TODO: add model diffusion
+	function SEIRS!(du, u, p ,t)
 		S, E, I, R = u
-		N = S + E + I + R
-		β, γ, σ, ξ, δ = p # inf, inf_per, exp_per, reinf_prob, death_rate
-		du[1] = dS = - (β * S * I) / N + ξ * R 
-		du[2] = dE = (β * S * I) / N - σ * E 
-		du[3] = dI = σ * E - γ * I 
-		du[4] = dR = γ * I - ξ * R
+		N = sum(du)
+		# β: rates of infection, γ: rates of recovery, σ: latency period 
+		# ω: immunity period, μ: birth and background death, α: virus mortality
+		β, γ, σ, ω, μ, α = p 
+		# birth - infection + lost immunity - natural death
+		du[1] = dS = μ*N - (β*I*S)/N + ω*R - μ*S
+		# infection - latency - natural death
+		du[2] = dE = (β*I*S)/N - σ*E - μ*E
+		# latenza - recovery - natural + epidemic death
+		du[3] = dI = σ*E - γ*I - (μ+α)*I
+		# recovery - lost immunity - natual death
+		du[4] = dR = γ*I - ω*R - μ*R
 	end
 
-	# u0 = [1.0-1E-7-4.0*1E-7, 4.0*1E-7, 1E-7, 0.0] # initial condition
-	# tspan = (0.0, 365.0) # ≈ 1 year
-	# p = [1/1.65, 1/18, 1/5.2, 1/6.67, 1/22.72]
+	# u0 = [1.0-1E-3, 1E-3, 0.0, 0.0] # initial condition
+	# tspan = (0.0, 1000.0) # ≈ 3 year
+	# p = [3/14, 1/14, 1/7, 1/365, 1/365*76, 0.044]
 
 	# prob = ODEProblem(seirs!, u0, tspan, p)
 	# sol = solve(prob, Tsit5())
 
-	# p1 = plot(sol, labels = [L"s" L"e" L"i" L"r" L"d"], title = "SEIRS Dynamics", lw = 2, xlabel = L"t")
+	# p1 = plot(sol, labels = [L"s" L"e" L"i" L"r"], title = "SEIRS Dynamics", lw = 2, xlabel = L"t")
 
 	function get_ODE_problem(f, u0, tspan, p)
 		return ODEProblem(f, u0, tspan, p)
