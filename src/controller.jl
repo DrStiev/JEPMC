f3905aacb98b2b6de1a72426f6dc18b431834eda
Matteo Stievano <mstievano9@gmail.com>
2023-04-20 14:32:24 +0200

    should start working on it

--- src/controller.jl
+++ src/controller.jl
@@ -1,11 +1,6 @@
 module controller
-    using ModelingToolkit
-	using DataDrivenDiffEq
-	using LinearAlgebra, DiffEqSensitivity, Optim
-	using DiffEqFlux, Flux
-	using Plots
-
-    include("ode.jl")
+	include("uode.jl")
+	include("graph.jl")
 
     # https://github.com/ChrisRackauckas/universal_differential_equations/blob/master/SEIR_exposure/seir_exposure.jl
 	# https://www.youtube.com/watch?v=5zaB1B4hOnQ
